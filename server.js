fec936c12fd823854355f43532c83168ad9772ba
Jobson Ribeiro <jobsonrisoa@gmail.com>
2021-12-11 13:48:42 -0300

    body-parser installed

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.listen(3333, ()=> console.log('listening on 3333'));
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+app.post('/quotes' , (req, res) => {
+    console.log('Heloo')
+})
+
+
+
+
