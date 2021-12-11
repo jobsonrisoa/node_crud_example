3a3ba3eae3fcc57ba52fe4e4c1c6321567800643
Jobson Ribeiro <jobsonrisoa@gmail.com>
2021-12-11 13:23:16 -0300

    getting index.html

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.listen(3333, ()=> console.log('listening on 3333'));
+
+app.get('/', (req, res)=> {
+    res.sendFile(__dirname + '/index.html')
+})
