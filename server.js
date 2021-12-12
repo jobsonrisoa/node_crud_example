de8162261f556c20b87568ef57d5124a81c31a1f
Jobson Ribeiro <jobsonrisoa@gmail.com>
2021-12-12 16:31:00 -0300

    Connection to MongoDB implemented

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
-app.listen(3333, ()=> console.log('listening on 3333'));
+MongoClient.connect('mongodb+srv://yoda:star123@cluster0.bdy0h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+                   {useUnifiedTopology: true})
+                   .then(client => {
+                       console.log('Connected to Database')
+                   })
+                   .catch(error => console.error(error))
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.listen(3000, ()=> console.log('listening on 3000'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/quotes' , (req, res) => {
-    console.log('Heloo')
+    console.log(req.body);
 })
 
 
