4c24c6169c13561e6772a0edace580fd48e934cc
Jobson Ribeiro <jobsonrisoa@gmail.com>
2021-12-13 12:30:45 -0300

    put taking json from main.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
-
 MongoClient.connect('mongodb+srv://yoda:star123@cluster0.bdy0h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
                    {useUnifiedTopology: true})
                    .then(client => {
@@ -11,12 +10,18 @@ MongoClient.connect('mongodb+srv://yoda:star123@cluster0.bdy0h.mongodb.net/myFir
                        const db = client.db('star-wars-quotes')
                        const quotesCollection = db.collection('quotes')
                        
+                        // ========================
+                        // Middlewares
+                        // ========================
                        app.set('view engine', 'ejs')
                        app.use(bodyParser.urlencoded({
                         extended: true
                        }))
+                       app.set('view engine', 'ejs')
+                       app.use(bodyParser.json())
+                       app.use(express.static('public'))
 
-                        app.set('view engine', 'ejs')
+                        //app.set('view engine', 'ejs')
                         app.get('/', (req, res) => {
                             db.collection('quotes').find().toArray()
                             .then(results => {
@@ -25,7 +30,7 @@ MongoClient.connect('mongodb+srv://yoda:star123@cluster0.bdy0h.mongodb.net/myFir
                             .catch(error => console.error(error))
                         })
                        
-                       app.set('view engine', 'ejs')
+                       //app.set('view engine', 'ejs')
                        app.post('/quotes' , (req, res) => {
                         quotesCollection.insertOne(req.body)
                         .then(result => {
@@ -34,6 +39,11 @@ MongoClient.connect('mongodb+srv://yoda:star123@cluster0.bdy0h.mongodb.net/myFir
                         .catch(error =>
                             console.error(error))
                        })
+
+                        //app.set('view engine', 'ejs')
+                        app.put('/quotes', (req, res) => {
+                            console.log(req.body)
+                        })
 
                        app.listen(3000, ()=> console.log('listening on 3000'))
                    })
